2ca33360be901a283790c3f9a3bf87a2ec5c37e1
navkelley <navkelley@gmail.com>
2016-12-21 01:54:01 -1000

    add root test-work in progress

--- test/test-server.js
+++ test/test-server.js
@@ -11,5 +11,14 @@ chai.use(chaiHttp);
 
 describe('Baby Steps', function() {
 	it('should reach root and deliver html', function(done) {
-		
-	}
+		chai.request(app)
+		.get('/')
+		.end(function(err, res) {
+			res.should.have.status(200); 
+			res.should.have.element('html');
+			res.should.have.element('p');
+			expect('Hello World!').to.equal('Hello World!');
+			done();
+		});
+	});
+});
