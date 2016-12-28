58b6cb4d723e6564f17aa012a42ade93cfa01533
Nicole Vianzon-Kelley <navkelley@gmail.com>
2016-12-28 00:16:43 -1000

    add nav for mobile

---
+++ nav.js
@@ -0,0 +1,12 @@
+/* Toggle between adding and removing the 
+"responsive" class to topnav when the user clicks on the icon */
+function toggleNav() {
+    var nav = document.getElementById("myTopnav");
+    if (nav.className === "topnav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "topnav";
+    }
+}
+
+export {toggleNav}; 
