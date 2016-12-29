e0067223a1cc480db06b9368c923d0b7d738aa9f
navkelley <navkelley@gmail.com>
2016-12-28 16:28:32 -1000

    code comment and cleanup

--- server.js
+++ server.js
@@ -3,26 +3,28 @@ var app = express();
 var path = require('path');
 app.use(express.static('public'));
 
+//use app.route to be able to reduce redunancy and typos
+//define route to login page
 app.route('/') 
 	.get(function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.hmtl'))
 })
-
+//define route to main dashboard
 app.route('/dashboard')
 	.get(function(req, res) {
 		res.sendFile(path.join(__dirname + '/public/dashboard.html'))
 })
-
+//define route to milestones and achievements
 app.route('/milestones')
 	.get(function(req, res) {
 		res.sendFile(path.join(__dirname + '/public/milestones.html'))
 })
-
+//define route to user forum 
 app.route('/forum')
 	.get(function(req, res) {
 		res.sendFile(path.join(__dirname + '/public/forum.html'))
 })
-
+//define route to user account settings 
 app.route('/user-account')
 	.get(function(req, res) {
 		res.sendFile(path.join(__dirname + '/public/user-account.html'))
@@ -30,5 +32,5 @@ app.route('/user-account')
 
 exports.app = app;
 
-console.log('Connected on port 8080'); 
 app.listen(process.env.PORT || 8080);
+console.log('Connected on port 8080');
