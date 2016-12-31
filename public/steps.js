c45bb8c93c38f02c4f79fe3d4db923f3d7e8738e
Nicole Vianzon-Kelley <navkelley@gmail.com>
2016-12-31 02:08:31 -1000

    add mock data for measurements

--- public/steps.js
+++ public/steps.js
@@ -23,3 +23,54 @@ var mock_narratives = {
         },
     ]
 };
+
+var mock_weight = {
+    "mock-weight": [
+        {
+            "date": "10/06/2016",
+            "weight": "8lbs 12oz",
+        },
+        {
+            "date": "10/25/2016",
+            "weight": "9lbs 8oz",
+        },
+        {
+            "date": "11/10/2016",
+            "weight": "11lbs 5oz",
+        },
+    ]
+};
+
+var mock_length = {
+    "mock-length": [
+        {
+            "date": "10/06/2016",
+            "length": "20.5 inches",
+        },
+        {
+            "date": "10/25/2016",
+            "length": "21 inches",
+        },
+        {
+            "date": "11/10/2016",
+            "length": "24 inches",
+        },
+    ]
+};
+
+var mock_headCir = {
+    "mock-headCir": [
+        {
+            "date": "10/06/2016",
+            "headCir": "13 inches",
+        },
+        {
+            "date": "10/25/2016",
+            "headCir": "15 inches",
+        },
+        {
+            "date": "11/10/2016",
+            "headCir": "16.5 inches",
+        },
+    ]
+};
