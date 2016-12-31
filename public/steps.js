2906b1ad50ba76a27454a852aaa200a986f40193
Nicole Vianzon-Kelley <navkelley@gmail.com>
2016-12-31 01:02:40 -1000

    add mock data for narrative

---
+++ public/steps.js
@@ -0,0 +1,25 @@
+var mock_narratives = {
+    "narratives": [
+        {
+            "title": "First night home",
+            "date": "10/08/2016",
+            "text": "First night home was long night. Not sure what went wrong",
+        },
+        {
+            "title": "Smiles",
+            "date": "10/31/2016",
+            "text": "Had the first smile today and it was great. Made the first night home not even matter."
+
+        },
+        {
+            "title": "Has cold",
+            "date": "11/5/2016",
+            "text": "baby is so congested today, thinking he has a cold",
+        },
+        {
+            "title": "Growing and eating well",
+            "date": "11/23/2016",
+            "text": "He is eating 4-6oz on average each meal! He has also outgrown his 3 month clothes!",
+        },
+    ]
+};
