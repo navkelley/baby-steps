b561e471fe134dcd6920f276e17a6bc36088c6ec
Nicole Vianzon-Kelley <navkelley@gmail.com>
2017-01-05 00:36:53 -1000

    code cleanup

--- server.js
+++ server.js
@@ -7,28 +7,28 @@ app.use(express.static('public'));
 //define route to login page
 app.route('/') 
 	.get(function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'))
-})
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
 //define route to main dashboard
 app.route('/dashboard')
 	.get(function(req, res) {
-		res.sendFile(path.join(__dirname + '/public/dashboard.html'))
-})
+		res.sendFile(path.join(__dirname + '/public/dashboard.html'));
+});
 //define route to milestones and achievements
 app.route('/milestones')
 	.get(function(req, res) {
-		res.sendFile(path.join(__dirname + '/public/milestones.html'))
-})
+		res.sendFile(path.join(__dirname + '/public/milestones.html'));
+});
 //define route to user forum 
 app.route('/forum')
 	.get(function(req, res) {
-		res.sendFile(path.join(__dirname + '/public/forum.html'))
-})
+		res.sendFile(path.join(__dirname + '/public/forum.html'));
+});
 //define route to user account settings 
 app.route('/user-account')
 	.get(function(req, res) {
-		res.sendFile(path.join(__dirname + '/public/user-account.html'))
-})
+		res.sendFile(path.join(__dirname + '/public/user-account.html'));
+});
 
 exports.app = app;
 
