92643c7b22b8b037503d198d57d7d12a7c68b60d
navkelley <navkelley@gmail.com>
2017-01-22 08:36:29 -1000

    remove mock data

--- src/js/steps.js
+++ src/js/steps.js
@@ -1,77 +1,5 @@
 $(document).ready(function() {
 
-    const data = {
-        "narratives": [
-            {
-                "title": "First night home",
-                "date": "10/08/2016",
-                "text": "First night home was long night. Not sure what went wrong",
-            },
-            {
-                "title": "Smiles",
-                "date": "10/31/2016",
-                "text": "Had the first smile today and it was great. Made the first night home not even matter."
-
-            },
-            {
-                "title": "Has cold",
-                "date": "11/5/2016",
-                "text": "baby is so congested today, thinking he has a cold",
-            },
-            {
-                "title": "Growing and eating well",
-                "date": "11/23/2016",
-                "text": "He is eating 4-6oz on average each meal! He has also outgrown his 3 month clothes!",
-            },
-        ],
-
-
-        "weight": [
-            {
-                "date": "10/06/2016",
-                "weight": "8lbs 12oz",
-            },
-            {
-                "date": "10/25/2016",
-                "weight": "9lbs 8oz",
-            },
-            {
-                "date": "11/10/2016",
-                "weight": "11lbs 5oz",
-            },
-        ],
-
-        "length": [
-            {
-                "date": "10/06/2016",
-                "length": "20.5 inches",
-            },
-            {
-                "date": "10/25/2016",
-                "length": "21 inches",
-            },
-            {
-                "date": "11/10/2016",
-                "length": "24 inches",
-            },
-        ],
-
-        "headCir": [
-            {
-                "date": "10/06/2016",
-                "headCir": "13 inches",
-            },
-            {
-                "date": "10/25/2016",
-                "headCir": "15 inches",
-            },
-            {
-                "date": "11/10/2016",
-                "headCir": "16.5 inches",
-            },
-        ]
-    };
-
     //to hold user id
     let currentUser; 
 
@@ -158,7 +86,6 @@ $(document).ready(function() {
             },
             success:(user) => {
                 currentUser = user._id;
-                console.log(user);
             },
         });
     };
@@ -181,7 +108,6 @@ $(document).ready(function() {
                 location.href = '/'
             },
             success: (user) => {
-                console.log(user);
                 getUserId(username); 
                 location.href = 'dashboard'
             },
